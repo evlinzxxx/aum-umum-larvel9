af65de67ff01d2313cf2141e98c226035c6dfbbc
evlinzxxx <evelinsitanggang@gmail.com>
2022-12-19 18:18:19 +0700

    first commit

--- app/Http/Controllers/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -20,21 +20,21 @@ class ChangePasswordController extends Controller
     {
         //validasi password baru dan lama
         $request->validate([
-            'old_password' =>'required',
+            'old_password' => 'required',
             'new_password' => 'required'
         ]);
 
         //jika password lama salah
-       if(!Hash::check($request->old_password, auth()->guard('user')->user()->password)){
+        if (!Hash::check($request->old_password, auth()->guard('user')->user()->password)) {
             return back()->with('failed', 'Password Lama Salah');
-       }
+        }
 
-       //jika data benar, update password
-       Siswa::where('nisn', auth()->user()->nisn)->update([
+        //jika data benar, update password
+        Siswa::where('nisn', auth()->user()->nisn)->update([
             'password' => Hash::make($request->new_password)
-       ]);
+        ]);
 
-       return back()->with('success', 'Password Berhasil Diubah');
+        return back()->with('success', 'Password Berhasil Diubah');
     }
 
     public function ubahPasswordGuru($nip)
@@ -45,24 +45,22 @@ class ChangePasswordController extends Controller
 
     public function updatePasswordGuru(Request $request)
     {
-         //validasi password baru dan lama
+        //validasi password baru dan lama
         $request->validate([
-            'old_password' =>'required',
+            'old_password' => 'required',
             'new_password' => 'required'
         ]);
 
-         //jika password lama salah
-       if(!Hash::check($request->old_password, auth()->guard('guru')->user()->password)){
+        //jika password lama salah
+        if (!Hash::check($request->old_password, auth()->guard('guru')->user()->password)) {
             return back()->with('failed', 'Password Lama Salah');
-       }
+        }
 
-       //jika data benar, update password
-       Guru::where('nisn', auth()->user()->nip)->update([
+        //jika data benar, update password
+        Guru::where('nip', auth()->user()->nip)->update([
             'password' => Hash::make($request->new_password)
-       ]);
+        ]);
 
-       return back()->with('success', 'Password Berhasil Diubah');
+        return back()->with('success', 'Password Berhasil Diubah');
     }
-
-
 }
